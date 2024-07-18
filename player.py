2fca4a150b2dea1ec49260a1fc769d60ddbeb900
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-17 22:04:32 -0700

    player.py

--- player.py
+++ player.py
@@ -1,7 +1,34 @@
+from turtle import Turtle
+
+## Constants ##
+
+# Turtle size
+WIDTH = 1.2
+HEIGHT = 1.2   # All turtles start out as 20 x 20
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("White")
+        self.goto(STARTING_POSITION)
+        self.shapesize(stretch_wid=HEIGHT, stretch_len=WIDTH)
+        self.color("Black")
+        self.left(90) # Turtle facing up
+
+
+    def go_up(self):
+        new_up = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_up)
+
+    def go_back(self):
+        new_up = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_up)
+
