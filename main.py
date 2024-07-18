332dd87ba1a4ad34081ebd26b48fd51373c9e61e
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-17 22:04:15 -0700

    main.py

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
 
+# Screen
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.tracer(0)
+screen.title("Turtle Crossing")
+screen.tracer(0)  # Turn off automatic screen updates
+
+# Initialize game elements after screen setup
+turt = Player()
+cars = CarManager()
+
+# Controls
+screen.listen()
+screen.onkey(turt.go_up, "Up")
+screen.onkey(turt.go_back, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.1)  # Code will refresh every 0.1 seconds
     screen.update()
