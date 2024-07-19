40e5a897a9b247c9da15f31331364fc735cdd155
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-18 21:10:55 -0700

    car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -1,23 +1,42 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 # Car size
-HEIGHT = 1
-LENGTH = 3
+WIDTH = 1
+LENGTH = 2
 
-RANDOM_Y = list(range(-300, 301))
+RANDOM_COLOR = random.choice(COLORS)
+
+CAR_X = 300
+CAR_Y = random.randrange(-300, 301)
 
 
-from turtle import Turtle
-import random
+class CarManager():
 
-class CarManager(Turtle):
+    def __init__(self, car_x, car_y, difficulty):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+        self.car_x = car_x  # Store the passed car_x value
+        self.car_y = car_y  # Store the passed car_y value
+        self.difficulty = difficulty  # Store the passed difficulty value
 
-    def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.penup()
-        self.color("Black")
-        self.shapesize(stretch_wid=HEIGHT, stretch_len=LENGTH)
-        self.goto(320,0)
+    def create_new_car(self):
+        chance = random.randint(1, self.difficulty)  # frequency of cars based on difficulty
+        if chance == 1:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_wid=WIDTH, stretch_len=LENGTH)
+            new_car.goto(300, random.randint(-250, 250))
+            self.all_cars.append(new_car)
+
+    def moving(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def increase_speed(self):
+        self.car_speed += MOVE_INCREMENT
